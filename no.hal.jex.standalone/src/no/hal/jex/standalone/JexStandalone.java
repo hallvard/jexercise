319b77762b1e24f0c3b53447d7508566deef134d
Hallvard Traetteberg <hal@idi.ntnu.no>
2012-03-03 23:26:37 +0100

    Added standalone class with main method

---
+++ no.hal.jex.standalone/src/no/hal/jex/standalone/JexStandalone.java
@@ -0,0 +1,10 @@
+package no.hal.jex.standalone;
+
+import no.hal.jex.swing.JexExercisePanel;
+
+public class JexStandalone {
+
+	public static void main(String[] args) {
+		JexExercisePanel.main(args);
+	}
+}
