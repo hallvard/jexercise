a48070f6fcbb7cd5c7a0975613538af2143c43a1
Hallvard Traetteberg <hal@idi.ntnu.no>
2013-12-17 16:06:33 +0100

    Runtime project and Confluence support.

--- no.hal.confluence.ui/src/no/hal/confluence/ui/actions/PostPasteHook.java
+++
@@ -1,9 +0,0 @@
-package no.hal.confluence.ui.actions;
-
-import java.util.Collection;
-
-import org.eclipse.jdt.core.IJavaElement;
-
-public interface PostPasteHook {
-	public void javaElementsPasted(Collection<IJavaElement> pastedJavaElements);
-}
