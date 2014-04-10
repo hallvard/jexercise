2c1c71b843ef4bcbe54940b22f8709eadc3fdb06
Hallvard Traetteberg <hal@idi.ntnu.no>
2014-04-10 11:04:40 +0200

    Oppdatert jexercise-runtime-lib

--- no.hal.jex.jextest.lib/src/no/hal/jex/jextest/extensions/JextestExtensions.java
+++ no.hal.jex.jextest.lib/src/no/hal/jex/jextest/extensions/JextestExtensions.java
@@ -1,21 +1,82 @@
 package no.hal.jex.jextest.extensions;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+
 public class JextestExtensions {
 
+	public static boolean operator_assertEquals(int actual, int expected) {
+		TestCase.assertEquals(expected, actual);
+		return true;
+	}
+	public static boolean operator_assertEquals(char actual, char expected) {
+		TestCase.assertEquals(expected, actual);
+		return true;
+	}
+	public static boolean operator_assertEquals(boolean actual, boolean expected) {
+		TestCase.assertEquals(expected, actual);
+		return true;
+	}	
+	public static boolean operator_assertEquals(Object actual, Object expected) {
+		TestCase.assertEquals(expected, actual);
+		return true;
+	}
+	
 	private static double epsilon = 0.0000001d;
-
-	public static boolean operator_approximatelyEquals(double n1, double n2) {
-		return n1 - epsilon <= n2 && n1 + epsilon >= n2;
+	
+	public static boolean operator_assertEquals(double actual, double expected) {
+		TestCase.assertEquals(expected, actual, epsilon);
+		return true;
 	}
 
 	private static boolean approximatelyEquals_trim = true;
 	private static boolean approximatelyEquals_ignoreCase = true;
 	
-	public static boolean operator_approximatelyEquals(String s1, String s2) {
+	public static boolean operator_assertEquals(String actual, String expected) {
 		if (approximatelyEquals_trim) {
-			s1 = s1.trim();
-			s2 = s2.trim();
+			actual = actual.trim();
+			expected = expected.trim();
 		}
-		return approximatelyEquals_ignoreCase ? s1.equalsIgnoreCase(s2) : s1.equals(s2);
+		if (approximatelyEquals_ignoreCase) {
+			actual = actual.toLowerCase();
+			expected = expected.toLowerCase();
+		}
+		TestCase.assertEquals(expected, actual);
+		return true;
+	}
+	
+	public static <T> boolean operator_assertEquals(Iterator<? extends T> actual, Iterable<? extends T> expected) {
+		return operator_assertEquals(actual, expected.iterator());
+	}
+
+	public static <T> boolean operator_assertEquals(Iterable<? extends T> actual, Iterator<? extends T> expected) {
+		return operator_assertEquals(actual.iterator(), expected);
+	}
+	
+	public static <T> boolean operator_assertEquals(Iterable<? extends T> actual, Iterable<? extends T> expected) {
+		return operator_assertEquals(actual.iterator(), expected);
+	}
+	
+	public static <T> boolean operator_assertEquals(Collection<? extends T> actual, Collection<? extends T> expected) {
+		if (actual.size() != expected.size()) {
+			throw new AssertionError("The collection is not of the expected length: " + expected.size());
+		}
+		return operator_assertEquals(actual.iterator(), expected.iterator());
+	}
+
+	public static <T> boolean operator_assertEquals(Iterator<T> actual, Iterator<T> expected) {
+		int count = 0;
+		while (true) {
+			if (actual.hasNext() != expected.hasNext()) {
+				throw new AssertionError("The iterator is not of the expected length: " + count);
+			}
+			if (! actual.hasNext()) {
+				break;
+			}
+			TestCase.assertEquals("Objects at position " + count + " differed", expected.next(), actual.next());
+		}
+		return true;
 	}
 }
