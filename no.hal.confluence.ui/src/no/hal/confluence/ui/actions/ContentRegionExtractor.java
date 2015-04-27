b8860ccf36f569c055db4d9b8b8dee8406455ded
Hallvard Traetteberg <hal@idi.ntnu.no>
2015-04-28 00:06:24 +0200

    Moved

--- no.hal.confluence.ui/src/no/hal/confluence/ui/actions/ContentRegionExtractor.java
+++
@@ -1,7 +0,0 @@
-package no.hal.confluence.ui.actions;
-
-import java.util.Collection;
-
-public interface ContentRegionExtractor<T> {
-	public Collection<T> getContentRegions(String browserContent);
-}
--- no.hal.confluence.ui/src/no/hal/confluence/ui/actions/SourceComposer.java
+++
@@ -1,5 +0,0 @@
-package no.hal.confluence.ui.actions;
-
-public interface SourceComposer<T> {
-	String composeSource(Iterable<T> regions);
-}
